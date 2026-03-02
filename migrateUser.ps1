937303b638414fdfaf2d0988c3b88d91426adce4
el-gran-pepe <135125063+el-gran-pepe@users.noreply.github.com>
2026-03-02 17:50:11 -0300

    Create migrateUser.ps1

---
+++ migrateUser.ps1
@@ -0,0 +1,15 @@
+Get-ADUser -Identity UsuarioNombre | Select UserPrincipalName, DisplayName, mail, fax, SamAccountName, ObjectGUID, @{name="ImmutableID"; expression={[Convert]::ToBase64String([guid]::New($_.ObjectGUID).ToByteArray())}}
+ 
+Connect-MgGraph -Scopes "User.Read.All"
+o
+Connect-MgGraph -Scopes "User.ReadWrite.All"
+
+Update-MgUser -UserId "usuario@empresa.com" -OnPremisesImmutableId "du0VS1jBZkujBMjAiZ5YlA=="
+
+###### Si falla, hacerlo desde módulo de AzureAD:
+
+Set-AzureADUser -ObjectId "usuario@empresa.com" -ImmutableId du0VS1jBZkujBMjAiZ5YlA==
+ 
+#Install-Module Microsoft.Graph -Scope CurrentUser
+
+#Import-Module Microsoft.Graph
